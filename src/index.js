3ae031ff1c30e12ede27486c9204017801a86656
Tilek Orozobekov <tilekusupbekov678@gmail.com>
2025-09-05 01:17:47 +0600

    finished program

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (matrix === undefined || !matrix) {
+    return [];
+  }
+  const res = [];
+  for (let i = 0; i < matrix.length; i += 1) {
+    if (i % 2 !== 0) {
+      matrix[i].reverse();
+    }
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      res.push(matrix[i][j]);
+    }
+  }
+  return res;
 };
